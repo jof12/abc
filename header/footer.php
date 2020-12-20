3252e45b4f619873ca4d449b111de3839b28a2d2
jof12 <72898201+jof12@users.noreply.github.com>
2020-12-20 22:52:25 +0800

    Add files via upload

---
+++ header/footer.php
@@ -0,0 +1,21 @@
+<!-- Awal Footer -->
+<hr class="footer">
+<div class="container">
+    <div class="row footer-body">
+        <div class="col-md-6">
+            <div class="copyright">
+                <strong>Copyright</strong> <i class="far fa-copyright"></i> 2020 - Designed by Kelompok RYJF</p>
+            </div>
+        </div>
+
+        <div class="col-md-6 d-flex justify-content-end">
+            <div class="icon-contact">
+                <label class="font-weight-bold">Follow Us </label>
+                <a href="https://m.facebook.com/Cafe-RYJF-115145987058067/" target="_blank"><img src="images/icon/fb.png" class="mr-3 ml-4" data-toggle="tooltip" title="Facebook"></a>
+                <a href="https://www.instagram.com/cafe_ryjf/" target="_blank"><img src="images/icon/ig.png" class="mr-3" data-toggle="tooltip" title="Instagram"></a>
+                <a href="https://twitter.com/CafeRyjf" target="_blank"><img src="images/icon/twitter.png" class="" data-toggle="tooltip" title="Twitter"></a>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Akhir Footer -->
---
+++ header/headerUser.php
@@ -0,0 +1,38 @@
+<!-- Jumbotron -->
+<div class="jumbotron jumbotron-fluid text-center">
+    <div class="container">
+        <h1 class="display-4"><span class="font-weight-bold">Cafe RYJF</span></h1>
+        <hr>
+        <p class="lead font-weight-bold">Silahkan Pesan Menu dan Nikmati Sensasinya. <br>
+            Eat Sleep Well</p>
+    </div>
+</div>
+<!-- Akhir Jumbotron -->
+
+<!-- Navbar -->
+<!-- Navbar -->
+<nav class="navbar navbar-expand-lg  bg-dark">
+    <div class="container">
+        <a class="navbar-brand text-white" href="user.php"><strong>Cafe</strong> RYJF</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link mr-4" href="user.php">HOME</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link mr-4" href="menu_pembeli.php">DAFTAR MENU</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link mr-4" href="pesanan_pembeli.php">PESANAN ANDA</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link mr-4" href="logout.php">LOGOUT</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<!-- Akhir Navbar -->
