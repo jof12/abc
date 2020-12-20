a8fb70286f6a933a12eba780445760586a86c492
jof12 <72898201+jof12@users.noreply.github.com>
2020-12-20 22:07:34 +0800

    Add files via upload

---
+++ admin.php
@@ -0,0 +1,82 @@
+<?php
+session_start();
+if (!isset($_SESSION['login_user'])) {
+  header("location: login.php");
+} else {
+?>
+
+  <!doctype html>
+  <html lang="en">
+
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="utama.css">
+    <link rel="stylesheet" type="text/css" href="fontawesome/css/all.min.css">
+
+    <title>Cafe RYJF</title>
+
+  <body>
+
+    <!-- Awal header -->
+    <?php
+    include('header/headerAdmin.php');
+    ?>
+    <!-- Akhir header -->
+
+    <!-- Menu -->
+    <div class="bgBawah">
+      <div class="container">
+        <div class="judul text-center">
+          <br>
+          <br>
+          <h3 class="font-weight-bold">Cafe RYJF</h3>
+          <br>Buka <strong>00:01 - 24:00</strong></h5>
+        </div>
+
+        <div class="row mb-10 mt-10 ">
+          <div class="col-md-6 d-flex justify-content-end">
+            <div class="card bg-dark text-white border-light">
+              <img src="img/buku.jpg" class="card-img" alt="Lihat Daftar Menu">
+              <div class="card-img-overlay mt-5 text-center">
+                <a href="daftar_menu.php" class="btn btn-primary">LIHAT DAFTAR MENU</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-6 d-flex justify-content-start">
+            <div class="card bg-dark text-white border-light">
+              <img src="img/kopi.jpg" class="card-img" alt="Lihat Pesanan">
+              <div class="card-img-overlay mt-5 text-center">
+                <a href="pesanan.php" class="btn btn-primary">LIHAT PESANAN</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Akhir Menu -->
+
+    <!-- Awal Footer -->
+    <?php
+    include('header/footer.php');
+    ?>
+    <!-- Akhir Footer -->
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/jquery.js"></script>
+  </body>
+
+  </html>
+<?php } ?>
